a13609367834ae355da491fa8ce203ae4c877086
Victor Tyavbee <68505976+Iamtsquare07@users.noreply.github.com>
2023-11-11 17:14:07 +0000

    Add files via upload

--- stopwatch.js
+++ stopwatch.js
@@ -1,7 +1,10 @@
 let isRunning = false;
 let startTime;
 let lapStartTime;
+let elapsedPausedTime = 0; // Track elapsed time when paused
 let laps = [];
+const lapList = document.getElementById("lapList");
+const resetBtn = document.getElementById("reset");
 
 function startStop() {
   if (isRunning) {
@@ -13,16 +16,20 @@ function startStop() {
 
 function start() {
   isRunning = true;
-  startTime = Date.now() - (lapStartTime || 0);
+  startTime = Date.now() - elapsedPausedTime - (lapStartTime || 0);
   lapStartTime = Date.now();
   update();
-  document.getElementById('startStopBtn').innerText = 'Stop';
+  document.getElementById("startStopBtn").innerText = "Stop";
+  resetBtn.innerText = "Reset";
+  resetBtn.style.display = "block";
 }
 
 function stop() {
   isRunning = false;
+  elapsedPausedTime += Date.now() - startTime;
   lapStartTime = null;
-  document.getElementById('startStopBtn').innerText = 'Start';
+  document.getElementById("startStopBtn").innerText = "Resume";
+  resetBtn.innerText = "Reset";
 }
 
 function lapReset() {
@@ -31,6 +38,7 @@ function lapReset() {
     const lapTime = Date.now() - lapStartTime;
     laps.push(formatTime(lapTime));
 
+    // Update lap list
     updateLapList();
 
     // Update lap start time
@@ -39,8 +47,9 @@ function lapReset() {
     // Reset the stopwatch
     startTime = null;
     lapStartTime = null;
+    elapsedPausedTime = 0;
     laps = [];
-    document.getElementById('output').innerText = formatTime(0);
+    document.getElementById("output").innerText = formatTime(0);
     updateLapList();
   }
 }
@@ -48,26 +57,41 @@ function lapReset() {
 function update() {
   if (isRunning) {
     const elapsedTime = Date.now() - startTime;
-    document.getElementById('output').innerText = formatTime(elapsedTime);
+    document.getElementById("output").innerText = formatTime(elapsedTime);
     requestAnimationFrame(update);
   }
 }
 
 function formatTime(time) {
   const date = new Date(time);
-  const hours = date.getUTCHours().toString().padStart(2, '0');
-  const minutes = date.getUTCMinutes().toString().padStart(2, '0');
-  const seconds = date.getUTCSeconds().toString().padStart(2, '0');
-  const milliseconds = Math.floor(date.getUTCMilliseconds() / 10).toString().padStart(2, '0');
+  const hours = date.getUTCHours().toString().padStart(2, "0");
+  const minutes = date.getUTCMinutes().toString().padStart(2, "0");
+  const seconds = date.getUTCSeconds().toString().padStart(2, "0");
+  const milliseconds = Math.floor(date.getUTCMilliseconds() / 10)
+    .toString()
+    .padStart(2, "0");
   return `${hours}:${minutes}:${seconds}.${milliseconds}`;
 }
 
 function updateLapList() {
-  const lapList = document.getElementById('lapList');
   lapList.innerHTML = "";
   laps.forEach((lap, index) => {
-    const lapItem = document.createElement('li');
+    const lapItem = document.createElement("li");
     lapItem.innerText = `Lap ${index + 1}: ${lap}`;
     lapList.appendChild(lapItem);
   });
 }
+
+function reset() {
+  laps = [];
+  lapList.textContent = "";
+  startTime = null;
+  lapStartTime = null;
+  elapsedPausedTime = 0;
+  if (isRunning) {
+    stop();
+  }
+  resetBtn.style.display = "none";
+}
+
+resetBtn.addEventListener("click", reset);
